9f63179892c7770185421977380df81ad4f64495
artem <artem_kisel04@mail.ru>
2023-09-06 10:02:20 +0300

    Update main.cpp

--- ImGui-Desktop-Sample/Source/main.cpp
+++ ImGui-Desktop-Sample/Source/main.cpp
@@ -1,6 +1,41 @@
 #include <Windows.h>
 
-int __stdcall wWinMain(HINSTANCE instance, HINSTANCE lastInstance, PWSTR args, int cmdShow)
+#include "ImGuiDesktop.h"
+#include "GUI.h"
+
+int __stdcall WinMain(HINSTANCE instance, HINSTANCE lastInstance, LPSTR lpCmdLine, int cmdShow)
 {
+	const char* name = "ImGui Desktop";
+
+	if (!ImGuiDesktop::Initialize(instance, name))
+	{
+		UnregisterClass(name, instance);
+		return EXIT_FAILURE;
+	}
+
+	auto active = true;
+
+	while (active)
+	{
+		MSG msg;
+		while (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+
+			if (msg.message == WM_QUIT)
+				active = false;
+		}
+
+		if (!active)
+			break;
+
+		ImGuiDesktop::Begin();
+		active = GUI::Render();
+		ImGuiDesktop::End();
+	}
+
+	ImGuiDesktop::Release();
+
 	return EXIT_SUCCESS;
 }
